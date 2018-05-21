2af6cbaaf1402a3afdb03280d482e926bc9dd322
Unknown <stormymtn@gmail.com>
2018-05-21 11:33:24 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ Animals/Assets/Scripts/Mammal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+
+public class Mammal : Animals {
+	public int HairCount = 1000;
+	public Color HairColor;
+	//public Color HairColor=Color.black;
+
+
+
+}
